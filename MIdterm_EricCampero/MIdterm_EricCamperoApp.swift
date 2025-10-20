bf37cd814717d5030255b9a4133cbeecaa446bd1
ecamper1-stack <ecamper1@my.smccd.edu>
2025-10-19 18:03:28 -0700

    Initial Commit

---
+++ MIdterm_EricCampero/MIdterm_EricCamperoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIdterm_EricCamperoApp.swift
+//  MIdterm_EricCampero
+//
+//  Created by Eric Campero on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MIdterm_EricCamperoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MIdterm_EricCamperoTests/MIdterm_EricCamperoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MIdterm_EricCamperoTests.swift
+//  MIdterm_EricCamperoTests
+//
+//  Created by Eric Campero on 10/19/25.
+//
+
+import Testing
+@testable import MIdterm_EricCampero
+
+struct MIdterm_EricCamperoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
